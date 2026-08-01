a54883436d7758eab0fd39196a07a188c119b598
sakharearyansurendra-web <sakharearyansurendra@gmail.com>
2026-08-01 19:15:52 +0530

    Delete eesa-website/eesa directory

--- eesa-website/eesa/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/config.php';
-if (is_logged_in()) {
-    audit($pdo, 'logout', current_user()['username']);
-}
-session_unset();
-session_destroy();
-redirect('/index.php');
