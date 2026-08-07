0323da0c4cd28c3abf38e45cd17f6b1238ee67ef
sakharearyansurendra-web <sakharearyansurendra@gmail.com>
2026-08-08 03:19:25 +0530

    Update join.php

--- eesa-website/eesa/pages/join.php
+++ eesa-website/eesa/pages/join.php
@@ -100,15 +100,21 @@ require __DIR__ . '/../includes/header.php';
             <span>Email</span>
           </label>
 
-          <div class="checkbox-row">
-            <label>
+     <div class="type-toggle-row">
+            <span>I am a</span>
+            <div class="type-toggle">
               <input type="checkbox" id="isAlumni" <?= $postedMemberType === 'alumni' ? 'checked' : '' ?>>
-              Alumni
-            </label>
-            <label>
+              <label for="isAlumni" class="toggle-chip">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 6L9 17l-5-5"/></svg>
+                Alumni
+              </label>
+
               <input type="checkbox" id="isFaculty" <?= $postedMemberType === 'faculty' ? 'checked' : '' ?>>
-              Faculty Member
-            </label>
+              <label for="isFaculty" class="toggle-chip">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 6L9 17l-5-5"/></svg>
+                Faculty
+              </label>
+            </div>
           </div>
 
           <input type="hidden" name="member_type" id="memberType" value="<?= h($postedMemberType) ?>">
